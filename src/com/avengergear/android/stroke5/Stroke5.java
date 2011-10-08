361f19665cc1549a73fc8b379b54166a54dc5fe1
Alex Lau <avengermojo@gmail.com>
2011-10-09 00:49:10 +0800

    Create Stroke5 KeyboardView in a Container

--- src/com/avengergear/android/stroke5/Stroke5.java
+++ src/com/avengergear/android/stroke5/Stroke5.java
@@ -32,16 +32,61 @@ import android.util.Log;
 
 public class Stroke5 extends InputMethodService implements KeyboardView.OnKeyboardActionListener
 {
+	private KeyboardContainer       mKeyboardContainer;
+	private KeyboardView            mStroke5KeyboardView;
+	private Stroke5Keyboard         mStroke5Keyboard;
+
 
 	/* Override the onCreate, to build the setup */ 
 	@Override public void onCreate() {
 		super.onCreate();
+		Log.d("Stroke5IME", "onCreate");
 	}
 
 	/**
+	 * InputMethodService Interface Started
+	 *
+	 * The following is InputMethod Abstract interface for call
+	 * back and option for implementation
+	 *
+	 **/
+
+	/**
 	 * Setting up the UI after onCreate and Configuration setup
+	 *
+	 * google:
+	 * for user-interface initialization, in particular to deal 
+	 * with configuration changes while the service is running.
+	 *
 	 **/
 	@Override public void onInitializeInterface() {
+		super.onInitializeInterface();
+		Log.d("Stroke5IME", "onInitializeInterface");
+		// create the acutal layout of the Stroke5 keyboard
+		mStroke5Keyboard = new Stroke5Keyboard(this, R.xml.stroke5);
+	} 
+
+	/**
+	 * Handle switching over from one client to another
+	 *
+	 * google:
+	 * to find out about switching to a new client.
+	 *
+	 **/
+	@Override public void onBindInput() {
+		super.onBindInput();
+		Log.d("Stroke5IME", "onBindInput");
+	}
+
+	/**
+	 * When client connected to the input method interface
+	 * 
+	 * google:
+	 * to deal with an input session starting with the client.
+	 *
+	 **/
+	@Override public void onStartInput(EditorInfo attribute, boolean restarting) {
+		super.onStartInput(attribute, restarting);
 	}
 
 	/**
@@ -51,17 +96,45 @@ public class Stroke5 extends InputMethodService implements KeyboardView.OnKeyboa
 	 **/
 	@Override public View onCreateInputView() {
 		Log.d("Stroke5IME", "onCreateInputView.");
-		return super.onCreateInputView();
+		mKeyboardContainer = (KeyboardContainer) getLayoutInflater().inflate(R.layout.keyboard_container, null);
+		mStroke5KeyboardView = (KeyboardView) mKeyboardContainer.getChildAt(0);
+		mStroke5KeyboardView.setOnKeyboardActionListener(this);
+		mStroke5KeyboardView.setKeyboard(mStroke5Keyboard);
+		return mKeyboardContainer;
 	}
 
+	/**
+	 * Create the actual candidates containre for user to pick
+	 * stroke5 candidates, stroke5 in general has lot of 
+	 * candidates for user to choice from, we need bigger service
+	 * area for older user to look and pick their choice
+	 *
+	 **/
 	@Override public View onCreateCandidatesView() {
 		Log.d("Stroke5IME", "onCreateCandidatesView.");
 		return super.onCreateCandidatesView();
 	}
 
-	@Override public void onStartInput(EditorInfo attribute, boolean restarting) {
-		super.onStartInput(attribute, restarting);
+	/**
+	 * Create UI for extractTextView 
+	 *
+	 * google:
+	 * Called by the framework to create the layout for showing
+	 * extacted text. Only called when in fullscreen mode. The 
+	 * returned view hierarchy must have an ExtractEditText 
+	 * whose ID is inputExtractEditText. 
+	 *
+	 **/ 
+	@Override public View onCreateExtractTextView(){
+		Log.d("Stroke5IME", "onCreateExtractTextView.");
+		return super.onCreateExtractTextView();
 	}
+
+
+	/**
+	 * InputMethodService Interface Ended
+	 *
+	 **/
 
 	public void onText(CharSequence text) {
 	}
---
+++ src/com/avengergear/android/stroke5/Stroke5Keyboard.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (C) 2011 AvengerGear Inc
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package com.avengergear.android.stroke5;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.inputmethodservice.Keyboard;
+
+import android.util.Log;
+
+
+public class Stroke5Keyboard extends Keyboard {
+
+	public Stroke5Keyboard(Context context, int xmlLayoutResId) {
+		super(context, xmlLayoutResId);
+		Log.d("Stroke5IME", "Stroke5Keyboard");
+	} 
+
+	public Stroke5Keyboard(Context context, int layoutTemplateResId,
+		CharSequence characters, int columns, int horizontalPadding) {
+		super(context, layoutTemplateResId, characters, columns, horizontalPadding);
+		Log.d("Stroke5IME", "Stroke5Keyboard - context");
+	}
+
+
+	static class Stroke5Key extends Keyboard.Key {
+		public Stroke5Key(Resources res, Keyboard.Row parent, int x, int y, XmlResourceParser parser) {
+			super(res, parent, x, y, parser);
+		}
+		/**
+		 * Overriding this method so that we can reduce the target area for the key that
+		 * closes the keyboard. 
+		 **/
+		@Override public boolean isInside(int x, int y) {
+			return super.isInside(x, codes[0] == KEYCODE_CANCEL ? y - 10 : y);
+		}
+	}
+}
+
